ea8ab0b9692275ca4c55a59fca6953e6d4010bc4
Diego <diego.perez@hotboards.org>
2013-08-29 18:34:39 -0700

    primeras versiones del dirver 6800 y hd44870 estos archivos son usados para controlar un lcd

---
+++ middleware/6800/_6800.h
@@ -0,0 +1,132 @@
+/*--------------------------------------------------------------------------------------------------
+  "THE BEER-WARE LICENSE" (Revision 42):
+  <phk@FreeBSD.ORG> wrote this file. As long as you retain this notice you
+  can do whatever you want with this stuff. If we meet some day, and you think
+  this stuff is worth it, you can buy me a beer in return to Hotboards crew
+  (Beer-ware license created by Poul-Henning Kamp)
+
+  \file         _6800.h
+  \author       Diego
+  \email        diego.perez@hotboards.org
+  \ver          1.0
+  \date         August 26, 2013
+  \target       8-bits Generic
+
+  \brief        Esta pieza de código emula la interfaz paralela 6800 mediante simples pines del uC,
+                las tres señales de control (E, RS y RW) utilizadas en esta interfaz se generan
+                mediante el driver gpios. La amplitud del bus solo puede ser de 4 u 8 bits y para
+                ello se utiliza un puerto completo del uC, en caso de 4 bits solo se usan los bits
+                mas significativos.
+ -------------------------------------------------------------------------------------------------*/
+#ifndef __6800_H__
+#define __6800_H__
+
+
+    /*-- Includes --*/
+    #include "types.h"
+    #include "gpios/gpios.h"
+     
+
+    /*-- Defines --*/
+    /**--------------------------------------------------------------------------------------------- 
+      \def      _6800_BUSLENGHT
+      \brief    tamaño del bus de datos (4 u 8)
+                Esta definicion es interna unicamente, actuando solo como valor por default cuando
+                cuando no se define _6800_BUSLENGHT en middleware_profile.h
+    ----------------------------------------------------------------------------------------------*/ 
+    //#define _6800_BUSLENGHT               4
+
+    /**---------------------------------------------------------------------------------------------
+      \def      _6800_ENABLE_TIME
+      \brief    Numero de nop instrucciones para completar 300ns utlizados en la generacion de la
+                señal Enable
+                Esta definicion es interna unicamente, actuando solo como valor por default cuando
+                cuando no se define _6800_ENABLE_TIME en middleware_profile.h
+    ----------------------------------------------------------------------------------------------*/
+    //#define _6800_ENABLE_TIME               1
+
+    /**--------------------------------------------------------------------------------------------- 
+      \def      _6800_DATAPORT
+      \brief    Selecciona el puerto del uC que sera utilizado como bus de datos, en caso de la opcion
+                de 4 bits, solo se utilizara la parte mas significativa.
+                Es obligatorio escribir esta definicion en el archivo middleware_profile.h
+    ----------------------------------------------------------------------------------------------*/ 
+    //#define _6800_DATAPORT              GPIOS_PORTD
+
+    /**--------------------------------------------------------------------------------------------- 
+      \def      6800_E_n 
+      \brief    pin que manipulara la señal de estrobo (enable)
+                Es obligatorio escribir esta definicion en el archivo middleware_profile.h
+    ----------------------------------------------------------------------------------------------*/ 
+    //#define _6800_E_P               	GPIOS_PORTC
+    //#define _6800_E_B               	6
+
+    /**--------------------------------------------------------------------------------------------- 
+      \def      6800_RW_n 
+      \brief    pin utilizado para indicar modo lectura o escritura
+                Es obligatorio escribir esta definicion en el archivo middleware_profile.h
+    ----------------------------------------------------------------------------------------------*/ 
+    //#define _6800_RW_P                  GPIOS_PORTC
+    //#define _6800_RW_B                  7
+
+    /**--------------------------------------------------------------------------------------------- 
+      \def      6800_RS_n 
+      \brief    pin itlizado para indicar si se escribe un dato o un comando
+                Es obligatorio escribir esta definicion en el archivo middleware_profile.h
+    ----------------------------------------------------------------------------------------------*/ 
+    //#define _6800_RS_P                  GPIOS_PORTB
+    //#define _6800_RS_B                  7
+
+    
+    /*-- Macros --*/
+    /**--------------------------------------------------------------------------------------------- 
+      \def      Define
+      \brief    Write a nice description for your macros
+    ----------------------------------------------------------------------------------------------*/ 
+    
+    
+    /*-- Functions --*/
+    /**---------------------------------------------------------------------------------------------
+      \brief      Inicializa los pines de control que actuaran como puerto paralelo
+      \param	  None
+      \return     None
+      \warning	  Se deben declarar los pines a usar en el archivo middleware_profile.h
+    ----------------------------------------------------------------------------------------------*/
+    void _6800_Init(void);
+
+    /**---------------------------------------------------------------------------------------------
+      \brief      Escribe un simple dato a través del bus (este valor puede ser de 8 o 4 bits)W/R=0,R/S=1
+      \param	  u8Data.- dato a ser enviado a través del bus de datos
+      \return     None
+      \warning	  En el caso de la opción de 4 bits la función solo toma en cuenta los 4 bits mas altos (4-7)
+    ----------------------------------------------------------------------------------------------*/
+    void _6800_WriteData(_U08 u8Data);
+
+    /**---------------------------------------------------------------------------------------------
+      \brief      lee un dato del dispositivo externo conectado al bus de datos (W/R=1, R/S=1)
+      \param	  None
+      \return     Dato leído
+      \warning	  En el caso de el modo de 4 bits solo se toma en cuenta los 4 bits mas altos (4-7)
+    ----------------------------------------------------------------------------------------------*/
+    _U08 _6800_u8ReadData(void);
+
+    /**---------------------------------------------------------------------------------------------
+      \brief      Esribe un comando al dispositivo externo (W/R=0 and R/S =0)
+      \param	  u8Cmd.- Comando a enviar
+      \return     None
+      \warning	  En el caso de el modo de 4 bits solo se toma en cuenta los 4 bits mas altos (4-7)
+    ----------------------------------------------------------------------------------------------*/
+    void _6800_WriteCommand(_U08 u08Cmd);
+
+    /**---------------------------------------------------------------------------------------------
+      \brief      Lee una dirección del dispositivo externo (W/R=1 and R/S =0)
+      \param	  None
+      \return     Dirección enviada
+      \warning	  En el caso de el modo de 4 bits solo se toma en cuenta los 4 bits mas altos (4-7).
+                  En este tipo de dispositivos no se leen comandos pero si direcciones internas.
+    ----------------------------------------------------------------------------------------------*/
+    _U08 _6800_u8ReadAddr(void);
+
+    
+#endif   
+ 
---
+++ modules/hd44780/hd44780.c
@@ -0,0 +1,217 @@
+/*--------------------------------------------------------------------------------------------------
+  "THE BEER-WARE LICENSE" (Revision 42):
+  <phk@FreeBSD.ORG> wrote this file. As long as you retain this notice you
+  can do whatever you want with this stuff. If we meet some day, and you think
+  this stuff is worth it, you can buy me a beer in return to Hotboards crew
+  (Beer-ware license created by Poul-Henning Kamp)
+
+  \file         hd44780.h
+  \author       Diego
+  \email        diego.perez@hotboards.org
+  \ver          1.0
+  \date         August 29, 2013
+  \target       8-bits Generic
+
+  \brief        Pieza de código para controlar un lcd alfanumérico externo con el controlador Hitachi
+                hd44780 o compatible. este código solo se encarga de incializar el lcd, así como mandar
+                comandos y/o datos al lcd, los detalles internos de comunicación se manejan a través
+                del código que maneja la interfaz 6800 en 4 u 8 bits.
+ -------------------------------------------------------------------------------------------------*/
+/*-- Includes --*/
+#include "hd44780.h"
+#include "6800/_6800.h"
+#include "delays/delays.h"
+#include "middleware_profile.h"
+#include "modules_profile.h"
+
+
+/*-- Private Definitions--*/
+#ifndef HD44780_ROWS
+    #define HD44780_ROWS                    2
+#endif
+
+#ifndef HD44780_COLUMNS
+    #define HD44780_COLUMNS                 16
+#endif
+
+/* Display ON/OFF Control defines */
+#define DON         		(_U08)0b00001111  /* Display on      */
+#define DOFF        		(_U08)0b00001011  /* Display off     */
+#define CURSOR_ON   		(_U08)0b00001111  /* Cursor on       */
+#define CURSOR_OFF  		(_U08)0b00001101  /* Cursor off      */
+#define BLINK_ON    		(_U08)0b00001111  /* Cursor Blink    */
+#define BLINK_OFF   		(_U08)0b00001110  /* Cursor No Blink */
+
+/* Cursor or Display Shift defines */
+#define SHIFT_CUR_LEFT    	(_U08)0b00000100  /* Cursor shifts to the left   */
+#define SHIFT_CUR_RIGHT   	(_U08)0b00000101  /* Cursor shifts to the right  */
+#define SHIFT_DISP_LEFT   	(_U08)0b00000110  /* Display shifts to the left  */
+#define SHIFT_DISP_RIGHT  	(_U08)0b00000111  /* Display shifts to the right */
+
+/* Function Set defines */
+#define FOUR_BIT   		(_U08)0b00101100  /* 4-bit Interface               */
+#define EIGHT_BIT  		(_U08)0b00111100  /* 8-bit Interface               */
+#define LINE_5X7   		(_U08)0b00110000  /* 5x7 characters, single line   */
+#define LINE_5X10  		(_U08)0b00110100  /* 5x10 characters               */
+#define LINES_5X7  		(_U08)0b00111000  /* 5x7 characters, multiple line */
+
+
+/*-- Global variables --*/
+#if HD44780_ROWS == 1 &&  HD44780_COLUMNS == 16
+	const _U08 au8Buffer[4]={0x00u,0x00u,0x00u,0x00u};
+#elif HD44780_ROWS == 2 &&  HD44780_COLUMNS == 16
+	const _U08 au8Buffer[4]={0x00u,0x40u,0x00u,0x00u};
+#elif HD44780_ROWS == 4 &&  HD44780_COLUMNS == 16
+	const _U08 au8Buffer[4]={0x00u,0x40u,0x14u,0x54u};
+#elif HD44780_ROWS == 2 &&  HD44780_COLUMNS == 32
+	const _U08 au8Buffer[4]={0x00u,0x40u,0x00u,0x00u};
+#elif HD44780_ROWS == 2 &&  HD44780_COLUMNS == 20
+	const _U08 au8Buffer[4]={0x00u,0x40u,0x00u,0x00u};
+#elif HD44780_ROWS == 2 &&  HD44780_COLUMNS == 40
+	const _U08 au8Buffer[4]={0x00u,0x40u,0x00u,0x00u};
+#elif HD44780_ROWS == 4 &&  HD44780_COLUMNS == 20
+	const _U08 au8Buffer[4]={0x00u,0x40u,0x14u,0x54u};
+#else
+	#error "configuration not supported"
+#endif
+	
+
+/*-- Private Macros --*/
+
+
+/*-- Private functions prototypes --*/
+
+
+/*-- External functions --*/
+/**-----------------------------------------------------------------------------------------------*/    
+void HD44780_Init(void)
+{
+    _6800_Init();
+    /*secuencia para incilizar controlador*/
+    Delays_ms((_U08)15);                /* 15 ms para que el controlador encienda */
+    _6800_WriteCommand((_U08)0x30);	/* Secuecia de encendido en modo 8 bits */
+    Delays_ms((_U08)5);
+    _6800_WriteCommand((_U08)0x30);
+    Delays_10us((_U08)20);
+    _6800_WriteCommand((_U08)0x30);
+    Delays_10us((_U08)4);
+
+    #if _6800_BUSLENGHT == 8
+        _6800_WriteCommand(0x38);           /*modo 8 bits todas las lineas activas*/
+        Delays_10us((_U08)4);
+        _6800_WriteCommand(DOFF);           /*Se apaga el controlador*/
+        Delays_10us((_U08)4);
+        _6800_WriteCommand((_U08)0x01);     /*se enciende y limpia el display*/
+        Delays_ms((_U08)2);
+        _6800_WriteCommand(SHIFT_DISP_LEFT);/*incremento de cursor a la derecha*/
+        Delays_10us((_U08)4);
+        _6800_WriteCommand(DON & CURSOR_OFF & BLINK_OFF); /*se despliega cursor y parpadeo*/
+        Delays_10us(4);
+        _6800_WriteCommand((_U08)0x80);                 /* Set Display data ram address to 0 */
+        Delays_10us(4);
+    #elif _6800_BUSLENGHT == 4
+        _6800_WriteCommand(0x20);
+        Delays_10us((_U08)4);
+        _6800_WriteCommand(0x20);           /*modo 8 bits todas las lineas activas*/
+        _6800_WriteCommand(0x80);
+        Delays_10us((_U08)4);
+        _6800_WriteCommand(DOFF);           /*Se apaga el controlador*/
+        _6800_WriteCommand(DOFF<<4u);
+        Delays_10us((_U08)4);
+        _6800_WriteCommand((_U08)0x01);     /*se enciende y limpia el display*/
+        _6800_WriteCommand((_U08)0x01<<4u);
+        Delays_ms((_U08)2);
+        _6800_WriteCommand(SHIFT_DISP_LEFT);/*incremento de cursor a la derecha*/
+        _6800_WriteCommand(SHIFT_DISP_LEFT<<4);
+        Delays_10us((_U08)4);
+        _6800_WriteCommand(DON & CURSOR_OFF & BLINK_OFF); /*se despliega cursor y parpadeo*/
+        _6800_WriteCommand((DON & CURSOR_OFF & BLINK_OFF)<<4u);
+        Delays_10us(4);
+        _6800_WriteCommand((_U08)0x80);                 /* Set Display data ram address to 0 */
+        _6800_WriteCommand((_U08)0x80<<4);
+        Delays_10us(4);
+    #endif
+}
+/**-----------------------------------------------------------------------------------------------*/    
+
+/**-----------------------------------------------------------------------------------------------*/    
+_BOOL HD44780_bBusyFlag(void)
+{
+    _U08 bFlag;
+    #if _6800_BUSLENGHT == 8
+        bFlag = (_BOOL)_6800_u8ReadAddr();
+    #elif _6800_BUSLENGHT == 4
+        bFlag = _6800_u8ReadAddr();
+        bFlag |= (_6800_u8ReadAddr()>>4);
+    #endif
+    return QUERY_8BIT(bFlag, 7);
+}
+/**-----------------------------------------------------------------------------------------------*/    
+
+/**-----------------------------------------------------------------------------------------------*/    
+void HD44780_SetCursor(const _U08 u8Row, const _U08 u8Col)
+{
+    _U08 u8Add;
+	
+    if((u8Row < HD44780_ROWS) && (u8Col < HD44780_COLUMNS))
+    {
+        u8Add = au8Buffer[u8Row] + u8Col;
+        #if _6800_BUSLENGHT == 8
+            _6800_WriteCommand(0x80 | u8Add);
+        #elif _6800_BUSLENGHT == 4
+            _6800_WriteCommand(0x80 | u8Add);
+            _6800_WriteCommand((0x80 | u8Add)<<4u);
+        #endif
+        Delays_10us(4);
+    }
+}
+/**-----------------------------------------------------------------------------------------------*/    
+
+/**-----------------------------------------------------------------------------------------------*/    
+void HD44780_WriteData(const _U08 u8Data)
+{
+    #if _6800_BUSLENGHT == 8
+        _6800_WriteData(u8Data);
+    #elif _6800_BUSLENGHT == 4
+        _6800_WriteData(u8Data);
+        _6800_WriteData(u8Data<<4);
+    #endif
+    Delays_10us(4);
+}
+/**-----------------------------------------------------------------------------------------------*/    
+
+/**-----------------------------------------------------------------------------------------------*/
+void HD44780_WriteCommand(const _U08 u8Data)
+{
+
+    #if _6800_BUSLENGHT == 8
+        _6800_WriteCommand(u8Data);
+    #elif _6800_BUSLENGHT == 4
+        _6800_WriteCommand(u8Data);
+        _6800_WriteCommand(u8Data<<4);
+    #endif
+    Delays_10us(4);
+}
+/**-----------------------------------------------------------------------------------------------*/
+
+/**-----------------------------------------------------------------------------------------------*/    
+void HD44780_WriteString(const rom _S08 *strString)
+{
+    while(*strString != 0)
+    {
+        HD44780_WriteData(*strString);
+        strString++;
+    }
+}
+/**-----------------------------------------------------------------------------------------------*/    
+
+/*-- Private functions --*/
+/**-------------------------------------------------------------------------------------------------    
+  \fn         HD44780_SetDDRamAddr
+  \brief      init the parallel master port internal registers 
+  \param	  None
+  \return     None
+  \warning	  None   	
+--------------------------------------------------------------------------------------------------*/
+
+
