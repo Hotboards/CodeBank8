14d2617c9a0b53c5500f2053492481b5ac31a80e
Diego Perez <diego.perez@hotboards.org>
2014-04-17 19:35:58 -0700

    version preliminar del driver 7segments -
    Version muy preliminar con el codigo terminado, aun falta probar toda su
    funcionalidad, pero ya es capaz de multiplexar los displays

---
+++ middleware/7segments/_7segments.c
@@ -0,0 +1,198 @@
+/*--------------------------------------------------------------------------------------------------
+  "THE BEER-WARE LICENSE" (Revision 42):
+  <phk@FreeBSD.ORG> wrote this file. As long as you retain this notice you
+  can do whatever you want with this stuff. If we meet some day, and you think
+  this stuff is worth it, you can buy me a beer in return to Hotboards crew
+  (Beer-ware license created by Poul-Henning Kamp)
+
+  \file         _6800.c
+  \author       Diego
+  \email        diego.perez@hotboards.org
+  \ver          1.0
+  \date         August 26, 2013
+  \target       8-bits Generic
+
+  \brief        Esta pieza de código emula la interfaz paralela 6800 mediante simples pines del uC,
+                las tres señales de control (E, RS y RW) utilizadas en esta interfaz se generan
+                mediante el driver **gpios**. La amplitud del bus solo puede ser de 4 u 8 bits y para
+                ello se utiliza un puerto completo del uC, en caso de 4 bits solo se usan los bits
+                mas significativos..
+ -------------------------------------------------------------------------------------------------*/
+/*-- Includes --*/
+#include "_7segments.h"
+#include "gpios/gpios.h"
+#include "hardware_profile.h"
+
+
+/*-- Private Definitions--*/
+#ifndef _7SEGMENTS_DIGI_N
+    #error "Se debe definir al menos un display en el archivo hardware_profile.h"
+//#define _7SEGMENTS_DIGI_N         1 definace como minimo en hardware_profile.h
+#endif
+
+#if _7SEGMENTS_DIGI_N > 4
+    #error "El numero maximo de displays permitido es 4"
+#endif
+
+#ifndef _7SEGMENTS_MODE
+    #define _7SEGMENTS_MODE                     0
+#endif
+#if _7SEGMENTS_MODE > 1
+    #error "Los valores permitidos de _7SEGMENTS_MODE son 0 o 1"
+#endif
+#define DIGI_OFF                                ~_7SEGMENTS_MODE
+#define DIGI_ON                                 _7SEGMENTS_MODE
+
+
+/*-- Global variables --*/
+static _U08 gau8Values[_7SEGMENTS_DIGI_N] = {0};
+static const _U08 gua8Numbers[10] = {_0,_1,_2,_3,_4,_5,_6,_7,_8,_9};
+
+
+/*-- Private Macros --*/
+
+
+/*-- Private functions prototypes --*/
+
+
+/*-- External functions --*/
+/**-----------------------------------------------------------------------------------------------*/
+void _7segments_Init(void)
+{
+    Gpios_WriteTris(_7SEGMENTS_PORT, 0x00);
+    Gpios_WritePin(_7SEGMENTS_DIGI0_P, _7SEGMENTS_DIGI0_B, DIGI_OFF);
+    #if _7SEGMENTS_DIGI_N > 1
+        Gpios_PinDirection(_7SEGMENTS_DIGI1_P, _7SEGMENTS_DIGI1_B, GPIOS_OUTPUT);
+        Gpios_WritePin(_7SEGMENTS_DIGI1_P, _7SEGMENTS_DIGI1_B, DIGI_OFF);
+    #endif
+    #if _7SEGMENTS_DIGI_N > 2
+        Gpios_PinDirection(_7SEGMENTS_DIGI2_P, _7SEGMENTS_DIGI2_B, GPIOS_OUTPUT);
+        Gpios_WritePin(_7SEGMENTS_DIGI2_P, _7SEGMENTS_DIGI2_B, DIGI_OFF);
+    #endif
+    #if _7SEGMENTS_DIGI_N > 3
+        Gpios_PinDirection(_7SEGMENTS_DIGI3_P, _7SEGMENTS_DIGI3_B, GPIOS_OUTPUT);
+        Gpios_WritePin(_7SEGMENTS_DIGI3_P, _7SEGMENTS_DIGI3_B, DIGI_OFF);
+    #endif
+}
+/**-----------------------------------------------------------------------------------------------*/
+
+/**-----------------------------------------------------------------------------------------------*/
+void _7segments_SetDisplay(const _U08 u8Display, const _U08 u8Value)
+{
+    if(u8Display < _7SEGMENTS_DIGI_N)
+    {
+        #if _7SEGMENTS_MODE == 1
+            gau8Values[u8Display] = ~u8Value;
+        #else
+            gau8Values[u8Display] = u8Value;
+        #endif
+    }
+}
+/**-----------------------------------------------------------------------------------------------*/
+
+/**-----------------------------------------------------------------------------------------------*/
+void _7segments_SetNumber(const _U16 u16Value)
+{
+    _U08 u8Digi;
+
+    u8Digi = (_U08)(u16Value % (_U16)1000); /*unidades*/
+    _7segments_SetDisplay(0, gua8Numbers[u8Digi]);
+    #if _7SEGMENTS_DIGI_N > 1
+        u8Digi = (_U08)((u16Value % (_U16)100) / (_U16)10); /*decenas*/
+        _7segments_SetDisplay(0, gua8Numbers[u8Digi]);
+    #endif
+    #if _7SEGMENTS_DIGI_N > 2
+        u8Digi = (_U08)((u16Value % (_U16)10) / (_U16)100); /*centenas*/
+        _7segments_SetDisplay(0, gua8Numbers[u8Digi]);
+    #endif
+    #if _7SEGMENTS_DIGI_N > 3
+        u8Digi = (_U08)(u16Value / (_U16)1000); /*millares*/
+        _7segments_SetDisplay(0, gua8Numbers[u8Digi]);
+    #endif
+}
+/**-----------------------------------------------------------------------------------------------*/
+
+/**-----------------------------------------------------------------------------------------------*/
+void _7segments_Task(void)
+{
+    static _U08 u8States = 0;
+
+    switch(u8States)
+    {
+        case 0:
+            Gpios_WritePin(_7SEGMENTS_DIGI0_P, _7SEGMENTS_DIGI0_B, DIGI_ON);
+            #if _7SEGMENTS_DIGI_N > 1
+                Gpios_WritePin(_7SEGMENTS_DIGI1_P, _7SEGMENTS_DIGI1_B, DIGI_OFF);
+            #endif
+            #if _7SEGMENTS_DIGI_N > 2
+                Gpios_WritePin(_7SEGMENTS_DIGI2_P, _7SEGMENTS_DIGI2_B, DIGI_OFF);
+            #endif
+            #if _7SEGMENTS_DIGI_N > 3
+                Gpios_WritePin(_7SEGMENTS_DIGI3_P, _7SEGMENTS_DIGI3_B, DIGI_OFF);
+            #endif
+            Gpios_WritePort(_7SEGMENTS_PORT, gau8Values[0]);
+            #if _7SEGMENTS_DIGI_N > 1
+                u8States = 1;
+            #else
+                u8States = 0;
+            #endif
+        break;
+        case 1:
+            Gpios_WritePin(_7SEGMENTS_DIGI0_P, _7SEGMENTS_DIGI0_B, DIGI_OFF);
+            #if _7SEGMENTS_DIGI_N > 1
+                Gpios_WritePin(_7SEGMENTS_DIGI1_P, _7SEGMENTS_DIGI1_B, DIGI_ON);
+            #endif
+            #if _7SEGMENTS_DIGI_N > 2
+                Gpios_WritePin(_7SEGMENTS_DIGI2_P, _7SEGMENTS_DIGI2_B, DIGI_OFF);
+            #endif
+            #if _7SEGMENTS_DIGI_N > 3
+                Gpios_WritePin(_7SEGMENTS_DIGI3_P, _7SEGMENTS_DIGI3_B, DIGI_OFF);
+            #endif
+            Gpios_WritePort(_7SEGMENTS_PORT, gau8Values[1]);
+            #if _7SEGMENTS_DIGI_N > 2
+                u8States = 2;
+            #else
+                u8States = 0;
+            #endif
+        break;
+        case 2:
+            Gpios_WritePin(_7SEGMENTS_DIGI0_P, _7SEGMENTS_DIGI0_B, DIGI_OFF);
+            #if _7SEGMENTS_DIGI_N > 1
+                Gpios_WritePin(_7SEGMENTS_DIGI1_P, _7SEGMENTS_DIGI1_B, DIGI_OFF);
+            #endif
+            #if _7SEGMENTS_DIGI_N > 2
+                Gpios_WritePin(_7SEGMENTS_DIGI2_P, _7SEGMENTS_DIGI2_B, DIGI_ON);
+            #endif
+            #if _7SEGMENTS_DIGI_N > 3
+                Gpios_WritePin(_7SEGMENTS_DIGI3_P, _7SEGMENTS_DIGI3_B, DIGI_OFF);
+            #endif
+            Gpios_WritePort(_7SEGMENTS_PORT, gau8Values[2]);
+            #if _7SEGMENTS_DIGI_N > 3
+                u8States = 3;
+            #else
+                u8States = 0;
+            #endif
+        break;
+        case 3:
+            Gpios_WritePin(_7SEGMENTS_DIGI0_P, _7SEGMENTS_DIGI0_B, DIGI_OFF);
+            #if _7SEGMENTS_DIGI_N > 1
+                Gpios_WritePin(_7SEGMENTS_DIGI1_P, _7SEGMENTS_DIGI1_B, DIGI_OFF);
+            #endif
+            #if _7SEGMENTS_DIGI_N > 2
+                Gpios_WritePin(_7SEGMENTS_DIGI2_P, _7SEGMENTS_DIGI2_B, DIGI_OFF);
+            #endif
+            #if _7SEGMENTS_DIGI_N > 3
+                Gpios_WritePin(_7SEGMENTS_DIGI3_P, _7SEGMENTS_DIGI3_B, DIGI_ON);
+            #endif
+            Gpios_WritePort(_7SEGMENTS_PORT, gau8Values[3]);
+            u8States = 0;
+        break;
+        default:
+            u8States = 0;
+        break;
+    }
+}
+/**-----------------------------------------------------------------------------------------------*/
+
+
+/*-- Private functions --*/
---
+++ middleware/7segments/_7segments.h
@@ -0,0 +1,133 @@
+/*--------------------------------------------------------------------------------------------------
+  "THE BEER-WARE LICENSE" (Revision 42):
+  <phk@FreeBSD.ORG> wrote this file. As long as you retain this notice you
+  can do whatever you want with this stuff. If we meet some day, and you think
+  this stuff is worth it, you can buy me a beer in return to Hotboards crew
+  (Beer-ware license created by Poul-Henning Kamp)
+
+  \file         _7segments.h
+  \author       Diego
+  \email        diego.perez@hotboards.org
+  \ver          1.0
+  \date         April 17, 2014
+  \target       8-bits Generic
+
+  \brief        Esta pieza de código controla el multiplexado de display de 7 segemtnos mediante un 
+                bus de datos comun. eL driver puede controlar de uno hasta cuatro display en
+                configuracion anodo comun o catodo comun, requiere de un puerto completo para el bus
+                de datos y un pin por cada display.
+                Es necesario mandar llamar una fucion periodica al menos cada 5ms para 4 displays.
+                Una simple funcion se encarga de setear los alorse que se desplegaran por display, lo
+                que pemrite desplegar cualquier caracter que se desee combiando los 7 segmentos
+                (mas un punto), donde los segmentos a = bit0, b = bit1, ..., g = bit6 y . = bit7
+                            a
+                           ---
+                        f | g | b
+                           ---
+                        e | d | c
+                           --- .
+ -------------------------------------------------------------------------------------------------*/
+#ifndef __7SEGMENTS__H__
+#define __7SEGMENTS__H__
+
+
+    /*-- Includes --*/
+    #include "types.h"
+     
+
+    /*-- Defines --*/
+    /**---------------------------------------------------------------------------------------------
+      \def      _7SEGMENTS_DIGI_N
+      \brief    Numero de digitos que conforman el display. maximo 4, minimo 1
+                Es obligatorio escribir esta definicion en el archivo hardware_profile.h
+    ----------------------------------------------------------------------------------------------*/
+    //#define _7SEGMENTS_DIGI_N                   1
+
+    /**---------------------------------------------------------------------------------------------
+      \def      _7SEGMENTS_MODE
+      \brief    Seleciona si los display son anodo(1) o catodo(0) comun, por default se toman como
+                catodo comun
+                Es obligatorio escribir esta definicion en el archivo hardware_profile.h
+    ----------------------------------------------------------------------------------------------*/
+    //#define _7SEGMENTS_MODE                     0
+
+    /**---------------------------------------------------------------------------------------------
+      \def      _7SEGMENTS_PORT
+      \brief    puerto completo que se encargara de mandar los datos a los display
+                Es obligatorio escribir esta definicion en el archivo hardware_profile.h
+    ----------------------------------------------------------------------------------------------*/
+    //#define _7SEGMENTS_PORT                     GPIOS_PORTB
+
+    /**--------------------------------------------------------------------------------------------- 
+      \def      _7SEGMENTS_DIGIx_n
+      \brief    pin utilizado para controlar cada uno de los digitos
+                Es obligatorio escribir esta definicion en el archivo hardware_profile.h
+    ----------------------------------------------------------------------------------------------*/ 
+    //#define _7SEGMENTS_DIGI0_P                  GPIOS_PORTD
+    //#define _7SEGMENTS_DIGI0_B                  4
+    //(repetir hasta el numero 3 si es necesario)
+
+    /**---------------------------------------------------------------------------------------------
+      \def      _e7SEGMENTS_NUMBERS
+      \brief    Valores que representan los numeros decimales desplegados en los displays
+    ----------------------------------------------------------------------------------------------*/
+    typedef enum
+    {
+        _0 = 0b00111111,
+        _1 = 0x00000110,
+        _2 = 0x01011011,
+        _3 = 0x01001111,
+        _4 = 0x01100110,
+        _5 = 0x01101101,
+        _6 = 0x01111101,
+        _7 = 0x00000111,
+        _8 = 0x01111111,
+        _9 = 0x01101111
+    }_e7SEGMENTS_NUMBERS;
+
+    
+    /*-- Macros --*/
+    /**--------------------------------------------------------------------------------------------- 
+      \def      Define
+      \brief    Write a nice description for your macros
+    ----------------------------------------------------------------------------------------------*/ 
+    
+    
+    /*-- Functions --*/
+    /**---------------------------------------------------------------------------------------------
+      \brief      Inicializa como salidas los pines que se usaran con el display
+      \param	  None
+      \return     None
+      \warning	  Se deben declarar los pines a usar en el archivo middleware_profile.h
+    ----------------------------------------------------------------------------------------------*/
+    void _7segments_Init(void);
+
+    /**---------------------------------------------------------------------------------------------
+      \brief      Carga el valor con los segemtnos a encender en cada display
+      \param	  u8Display.- numero de display uqe desplegara el valor seleccionado
+      \param	  u8Value.- valor con los segmentos que se deberan encender
+      \return     None
+      \warning	  Un bit en uno represetna un segmento encendido. La funcion invertira el valor si el
+                  el driver esta configurado como anodo comun.
+    ----------------------------------------------------------------------------------------------*/
+    void _7segments_SetDisplay(const _U08 u8Display, const _U08 u8Value);
+
+    /**---------------------------------------------------------------------------------------------
+      \brief      Despliega un numero decimal de hasta 4 digitos en los displays
+      \param	  u16Value.- numero a desplegar en los displays
+      \return     None
+      \warning	  La funcion toma en cuenta el numero de displays que se ha configurado. No se toma
+                  en cuenta el punto
+    ----------------------------------------------------------------------------------------------*/
+    void _7segments_SetNumber(const _U16 u16Value);
+
+    /**---------------------------------------------------------------------------------------------
+      \brief      Actualiza el valor en los display y multiplexa al siguiente display
+      \param	  None
+      \return     None
+      \warning	  Esta funcion se deb mandar llamar de manera periodica al menos cada 5ms
+    ----------------------------------------------------------------------------------------------*/
+    void _7segments_Task(void);
+
+    
+#endif   
